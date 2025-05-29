d97bb988efaab375c26c1b135863aecf08a2f021
Anish Thakur <123861384+Anish09122@users.noreply.github.com>
2025-05-29 09:25:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const apiKey="f1feca358244d4fd6aa300fcc9c1b36b";
 const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchbox=document.querySelector(".search input");
 const searchbtn=document.querySelector(".search button");
+const hide==document.querySelector(".weather");
 async function checkWeather(city){
 	const Response= await fetch(apiUrl+ city +`&appid=${apiKey}`);
 	var data=await Response.json();
 	console.log(data);
-
+        hide.style.display='block';
 	document.querySelector(".city").innerHTML=data.name;
 	document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+ "°C";
 	document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
